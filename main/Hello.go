b02647584d6fbda4c435df13db5863058696faae
yiyanghua <Nicaiba!@#1>
2018-05-14 20:25:02 +0800

    test

--- main/Hello.go
+++ main/Hello.go
@@ -1,22 +1,67 @@
 package main
 
 import (
+	"errors"
+	"ice-go"
+	"path/filepath"
 	"fmt"
-	"sync"
-)
-var (
-	serverContextMap   = make(map[string]*string, 8)
-	serverContextMutex sync.Mutex
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
 )
 
-func main()  {
-	fmt.Print("hello")
-	var confFile string
-	confFile = "serverdemo"
-	ms := serverContextMap[confFile]
-	if ms==nil {
-		fmt.Println("---")
-	}else {
-		fmt.Println("####")
+type TestFun func(url string) string
+
+type DefaultFactory struct {
+	test map[string]TestFun
+}
+
+func (factory *DefaultFactory) regist(key string, fun TestFun) {
+	factory.test[key] = fun
+}
+
+func main() {
+	ice.Test()
+	readFromyaml("main/serverdemo.yaml")
+
+	m := make(map[string]TestFun, 2)
+	factory := DefaultFactory{test: m}
+
+	factory.regist("1111", func(url string) string {
+		return url;
+	})
+
+	factory.regist("2222", func(url string) string {
+		return url;
+	})
+
+	test := "test12";
+	if testFun, ok := factory.test["1111"]; ok {
+		value := testFun(test);
+		fmt.Printf("########%s\n", value)
 	}
+
+}
+
+func readFromyaml(path string) error {
+	filename, err := filepath.Abs(path)
+	if err != nil {
+		fmt.Printf("can not find the file:%s\n", filename)
+		return errors.New("can not find the file :" + filename)
+	}
+
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("read config file fail. file: %s, error: %s\n", filename, err.Error())
+		return errors.New("read config file fail. " + err.Error())
+	}
+
+	m := make(map[string]interface{}, 32)
+
+	yaml.Unmarshal([]byte(data), &m)
+
+	for k, v := range m {
+		fmt.Printf("%s=%s \n", k, v)
+	}
+
+	return nil
 }
---
+++ server.go
@@ -0,0 +1,9 @@
+package ice
+
+import (
+	"fmt"
+)
+
+func Test()  {
+	fmt.Println("----")
+}
