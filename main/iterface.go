ca563be84a699198a4bfe8d0f98c6c189e8e3a7a
yiyanghua <yiyanghua@gmail.com>
2019-11-06 10:02:29 +0800

    example

---
+++ main/iterface.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"sync"
+	"fmt"
+)
+
+type Host interface {
+	// Health checks whether the host is healthy or not
+	Health() bool
+}
+
+// types.ClusterManager
+type clusterManager struct {
+	name 	string
+	mux     sync.Mutex
+}
+
+func (cm *clusterManager) test()  {
+
+}
+
+func (cm *clusterManager) Health(name string) bool {
+	fmt.Println(name)
+	return false
+}
+
+type A struct {
+	clusterManager
+}
+
+func main()  {
+	a :=&A{}
+	a.Health("aa")
+}
