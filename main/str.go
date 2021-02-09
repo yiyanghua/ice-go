946f88c31c63e136a8f56894c3c10189b98c5daf
yiyanghua <yiyanghua@gmail.com>
2021-02-09 15:40:44 +0800

    ss

--- main/str.go
+++ main/str.go
@@ -5,6 +5,16 @@ import "fmt"
 type s string
 
 func main() {
+	// https://www.slideshare.net/haoel/go-programming-patterns
+	a := make([]int,32)
+	b := a[1:16]
+	a = append(a,1)
+	a[2] =42
+
+	fmt.Println(b[0])
+	fmt.Println(b[1])
+	fmt.Println(b[2])
+
 	var d string
 	d = "aa"
 	fmt.Println(s(d))
