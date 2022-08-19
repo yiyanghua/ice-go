01300826edd90952cd3af95a4153856e78bba493
yiyanghua <yiyanghua@jd.com>
2022-08-19 11:30:31 +0800

    http

---
+++ net/handler/http.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+)
+
+type name struct {
+}
+
+func (n name) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	r.Cookies()
+	w.Write([]byte("11111"))
+}
+
+func hf(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hf"))
+}
+
+func main() {
+	mx := http.NewServeMux()
+	n := &name{}
+	mx.Handle("/aa", n)
+
+	http.HandleFunc("/hf", hf)
+	http.Handle("aa", http.HandlerFunc(hf))
+
+	http.ListenAndServe(":8080", nil)
+}
