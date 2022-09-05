17f61003a205b039232e949931c87e59d6ae7ce7
yiyanghua <yiyanghua@jd.com>
2022-09-05 18:06:54 +0800

    refactor

--- time/timeout.go
+++ time/timeout.go
@@ -12,20 +12,22 @@ func main() {
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
 		go func(num int, close <-chan struct{}) {
-			defer wg.Done()
+			defer func() {
+				wg.Done()
+			}()
 			<-close
 			fmt.Println(num)
 		}(i, c)
 	}
 
-	if WaitTimeout(&wg, time.Second*2) {
+	if waitTimeout(&wg, time.Second*2) {
 		close(c)
 		fmt.Println("timeout exit")
 	}
 	time.Sleep(time.Second * 10)
 }
 
-func WaitTimeout(wg *sync.WaitGroup, timeout time.Duration) bool {
+func waitTimeout(wg *sync.WaitGroup, timeout time.Duration) bool {
 	// 要求手写代码
 	// 要求sync.WaitGroup支持timeout功能
 	// 如果timeout到了超时时间返回true
