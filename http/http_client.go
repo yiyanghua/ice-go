7786a7bddc7bf7dc46df16b20ef1f30ef0280c84
yiyanghua <yiyanghua@jd.com>
2024-01-25 16:19:35 +0800

    add http

---
+++ http/http_client.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+func res2string(res *http.Response, err error) string {
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	d, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return string(d)
+}
+
+func get(url string) string {
+	res, err := http.Get(url)
+	return res2string(res, err)
+}
+
+func do(uri string) string {
+	client := &http.Client{}
+
+	u := &url.URL{
+		Scheme: "http",
+		Host:   uri,
+	}
+	req := &http.Request{
+		URL: u,
+	}
+	client.Do(req)
+
+	return res2string(client.Do(req))
+}
+
+func main() {
+	fmt.Println(get("https://www.baidu.com/"))
+	fmt.Println("--------------------------------")
+	fmt.Println(do("www.baidu.com"))
+}
